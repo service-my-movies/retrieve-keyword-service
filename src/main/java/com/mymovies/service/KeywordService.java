c26e03954ec15b6f8961bb1dc4e48018c27f0065
Ranushan <ranusha.n@hotmail.fr>
2020-04-30 20:45:54 +0200

    [ORIGIN][MASTER][UPDATED] Add Dockerfile dependancy file centralize config

--- src/main/java/com/mymovies/service/KeywordService.java
+++ src/main/java/com/mymovies/service/KeywordService.java
@@ -22,6 +22,9 @@ public class KeywordService implements IKeywordService {
 	
 	@Value("${resource.api.language}")
 	private String Language;
+	
+	@Value("${service.url}")
+	private String serviceUrl;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(KeywordService.class);
 	
@@ -31,9 +34,13 @@ public class KeywordService implements IKeywordService {
 	public KeywordsDTO getAPI_Keyword(String movie_id) {
 
 		KeywordsDTO listOfKeywords = null;
+		
+		String url = serviceUrl == "" ? BASE_URL+movie_id+"/keywords"+API_KEY+Language : serviceUrl+movie_id;
+		
+		LOGGER.info("@Get getAPI_Keyword Service URL : " + url);
 
 		try {
-			listOfKeywords = restTemplate.getForObject(BASE_URL+movie_id+"/keywords"+API_KEY+Language, KeywordsDTO.class);
+			listOfKeywords = restTemplate.getForObject(url, KeywordsDTO.class);
 		} catch (Exception e) {
 			LOGGER.error("Unexpected Error From Service: getAPI_Keyword: " + e);
 		}
